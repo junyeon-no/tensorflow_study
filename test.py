75ac54c1ba20d7642de04a7d48e0ad7a3f31e1ca
junhyun <whwns5203@naver.com>
2020-09-16 07:17:02 +0900

    2020-09-16 study_ roccurve

---
+++ test.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import sys
+a = tf.Variable(1)
+b = tf.Variable(2)
+print('-------------------------------')
+print(a + b)
+print(sys.version)
+print('-------------------------------')
